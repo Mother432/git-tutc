bcc8e7765dd249a553809262d3071efcc234bd00
sandhya <you@example.com>
2024-06-30 22:02:26 +0530

    commiting code and Hello.c

---
+++ Hello.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void padString(char *str, int totalWidth) {
+    int len = strlen(str);
+    
+    if (len >= totalWidth) {
+        printf("%s\n", str); // No padding needed if string length >= totalWidth
+    } else {
+        int numStars = totalWidth - len;
+        for (int i = 0; i < numStars; i++) {
+            printf("*");
+        }
+        printf("%s\n", str);
+    }
+}
+
+int main() {
+    char str[] = "Hello";
+    int totalWidth = 10;
+    
+    padString(str, totalWidth);
+    
+    return 0;
+}
